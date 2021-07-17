adf568bec4b85c922804b2de777cc350be33c603
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-07-17 10:50:24 +0700

    Update ap-install.sh

--- ap-install.sh
+++ ap-install.sh
@@ -3,13 +3,11 @@
 . /etc/init.d/tc-functions
 . /var/www/cgi-bin/pcp-functions
 useBusybox
-
 cd /tmp/
 wget https://raw.githubusercontent.com/lovehifi/picoreaddon/main/picoreaddon.tgz
 tar -zxf /tmp/picoreaddon.tgz --overwrite -C /home/tc/
-
+sleep 2
 pcp_write_var_to_config USER_COMMAND_2 "%2fbin/sleep%2010;%2fhome%2ftc%2faplayer%2faplayer.sh;%2fhome%2ftc%2faprenderer%2faprenderer.sh;%2fhome%2ftc%2fapmenu%2fapmenu.sh"
-
 echo "Rebooting..."
 sleep 3
 pcp br
