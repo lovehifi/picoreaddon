13ca0e4f39f817110a3434100a95ab1e437e292b
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-07-17 09:57:40 +0700

    Add files via upload

--- ap-install.sh
+++ ap-install.sh
@@ -4,7 +4,7 @@
 . /var/www/cgi-bin/pcp-functions
 useBusybox
 
-cd /tmp/$TARGET
+cd /tmp/
 wget https://raw.githubusercontent.com/quatmo/piCoreAddon/main/APMenu.tgz
 tar -zxf /tmp/APMenu.tgz --overwrite -C /home/tc/
 pcp_write_var_to_config USER_COMMAND_2 "sleep 10;/home/tc/aplayer/aplayer.sh;/home/tc/apmenu/apmenu.sh;/home/tc/aprenderer/aprenderer.sh"
