41f07d0a2fd7bb1f1f9327240db3590d61cab850
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-07-17 10:00:36 +0700

    Update ap-install.sh

--- ap-install.sh
+++ ap-install.sh
@@ -5,7 +5,7 @@
 useBusybox
 
 cd /tmp/
-wget https://raw.githubusercontent.com/quatmo/piCoreAddon/main/APMenu.tgz
+wget https://raw.githubusercontent.com/lovehifi/picoreaddon/main/APMenu.tgz
 tar -zxf /tmp/APMenu.tgz --overwrite -C /home/tc/
 pcp_write_var_to_config USER_COMMAND_2 "sleep 10;/home/tc/aplayer/aplayer.sh;/home/tc/apmenu/apmenu.sh;/home/tc/aprenderer/aprenderer.sh"
 
